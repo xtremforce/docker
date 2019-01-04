925033d86dd2d56057e7f022b07c4d57f97ea43e
X-Force <xtremforce@gmail.com>
2019-01-05 01:40:40 +0800

    Fix errors

--- ss-privoxy/run.sh
+++ ss-privoxy/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-privoxy --no-daemon /conf/privoxy.txt
-ss-server -c /conf/shadowsocks.json
+privoxy --no-daemon /conf/privoxy.conf
+#ss-server -c /conf/ss-server.json
+ss-local -c /conf/ss-local.json

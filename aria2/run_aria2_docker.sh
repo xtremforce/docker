77a73ba5cc1683f9a4faf3918ec2352d1f98ee23
X-Force <xtremforce@gmail.com>
2024-04-18 18:37:42 +0800

    Update run_aria2_docker.sh

--- aria2/run_aria2_docker.sh
+++ aria2/run_aria2_docker.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+#判断 Env 中是否存在 PGID 和 PUID
+if [[ -z "${PUID}" ]]; then
+	PUID=1026
+fi
+if [[ -z "${PGID}" ]]; then
+	PGID=100
+fi
+if [[ -z "${UMASK}" ]]; then
+	UMASK=022
+fi
+
 if [[ ! -f "/conf/aria2.session" ]]; then
 	touch /conf/aria2.session
 fi
@@ -8,6 +19,19 @@ if [[ ! -f "/conf/aria2.log" ]]; then
 	touch /conf/aria2.log
 fi
 
-#THIS IS DOCKER VERSION
-umask 0000
-aria2c --conf-path=/conf/aria2.conf
+# 设置文件权限
+chown -R ${PUID}:${PGID} /conf
+chown -R ${PUID}:${PGID} /download
+
+# 设置aria2运行用户
+if [ ${PUID} -eq "0" ]
+then
+    USER=root
+else
+    USER=aria2
+    groupmod -o -g "$PGID" "$USER"
+    usermod -o -u "$PUID" -g "$PGID" "$USER"
+fi
+
+umask ${UMASK}
+sudo -u#${PUID} /usr/bin/aria2c --conf-path=/conf/aria2.conf
